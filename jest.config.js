27fe7e0e3d9d016e6de87c6050dc6c415182c316
Victor Salles <vsallesa@gmail.com>
2023-03-02 12:44:01 -0300

    initial instruction: delete solution code

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  moduleNameMapper: {
-    '^utils/(.*)$': '<rootDir>/src/utils/$1',
-    '^database/(.*)$': '<rootDir>/src/database/$1',
-    '^constants/(.*)$': '<rootDir>/src/constants/$1',
-    '^middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
-    '^operations/(.*)$': '<rootDir>/src/operations/$1',
-  }
-}
